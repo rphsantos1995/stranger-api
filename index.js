37f4d95a7642a06de3680f6cb18eaf49e2a07b0c
rphsantos1995 <rph.santos@Outlook.com>
2022-03-13 01:03:12 -0300

    port heroku env

--- index.js
+++ index.js
@@ -30,6 +30,8 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(process.env.PORT, () => {
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
   console.log('Escutando na porta 3000');
 });
